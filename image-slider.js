fb1b647e91b0c425b9f5d726258e4b3c89b5c5d8
Gwen Tripet-Costet <gwenden31@gmail.com>
2025-07-18 14:49:00 +0200

    sfjef

--- image-slider.js
+++ image-slider.js
@@ -7,6 +7,7 @@ H5P.ImageSlider = (function ($) {
   function C(options, id) {
     this.$ = $(this);
     var self = this;
+    console.log('this is test234');
     H5P.EventDispatcher.call(this);
     // Extend defaults with provided options
     this.options = $.extend(true, {}, {
